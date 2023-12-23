86ca1377d279f452d26b4e7d36fcc440ac03666c
Praneel Hawaldar <praneel.kh@gmail.com>
2023-12-22 19:11:57 -0500

    COMP 110 Exercises

--- exercises/ex10/data_utils.py
+++ exercises/ex10/data_utils.py
@@ -1,5 +1,72 @@
 """Dictionary related utility functions."""
 
-__author__ = ""
+__author__ = "730663941"
 
-# Define your functions below
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read csv file and return as a list of dicts with the headers as the keys."""
+    result: list[dict[str, str]] = []
+    file_handle = open(filename, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        result.append(row)
+    file_handle.close()
+    return result
+
+
+def column_values(table: list[dict[str, str]], header: str) -> list[str]:
+    """Return a list of all values under a specific header."""
+    result: list[str] = []
+    # loop through each element (dictionary) of the list
+    for elem in table:
+        # for each dictionary (elem), get the value at key "header" and add that to result
+        result.append(elem[header])
+    return result
+
+
+def columnar(table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Reformat data so it's a dictionary with column headers as keys."""
+    result: dict[str, list[str]] = {}
+    # loop through keys of one row of the table to get the headers
+    first_row: dict[str, str] = table[0]
+    for key in first_row:
+        # for each key (header), make a dictionary entry with all the column values
+        result[key] = column_values(table, key)
+    return result
+
+
+def head(table: dict[str, list[str]], num_rows: int) -> dict[str, list[str]]:
+    """Produce a new column-based table with only the first n rows of data for each column."""
+    result: dict[str, list[str]] = {}
+    for column in table:
+        if num_rows >= len(table[column]):
+            return table
+        else:
+            stored_vals: list[str] = []
+            for i in range(num_rows):
+                stored_vals.append(table[column][i])
+            result[column] = stored_vals
+    return result
+
+
+def select(table: dict[str, list[str]], column_names: list[str]) -> dict[str, list[str]]:
+    """Produce a new column-based table with only a specific subset of the original columns."""
+    result: dict[str, list[str]] = {}
+    for name in column_names:
+        result[name] = table[name]
+    return result
+
+
+def concat(table1: dict[str, list[str]], table2: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Produce a new column-based table with two column-based tables combined."""
+    result: dict[str, list[str]] = {}
+    for column_t1 in table1:
+        result[column_t1] = table1[column_t1]
+    for column_t2 in table2:
+        if column_t2 in result:
+            result[column_t2].extend(table2[column_t2])
+        else:
+            result[column_t2] = table2[column_t2]
+    return result
